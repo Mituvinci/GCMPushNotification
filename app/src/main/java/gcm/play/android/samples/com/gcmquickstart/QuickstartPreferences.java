2dcaeb64ea9b3643382d46a5e6f52b9595d2a6a7
mitu <catvinchy@gmail.com>
2016-05-15 15:43:51 +0600

    Initial commit

---
+++ app/src/main/java/gcm/play/android/samples/com/gcmquickstart/QuickstartPreferences.java
@@ -0,0 +1,10 @@
+package gcm.play.android.samples.com.gcmquickstart;
+
+/**
+ * Created by mitu on 5/15/16.
+ */
+public class QuickstartPreferences {
+
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+}
